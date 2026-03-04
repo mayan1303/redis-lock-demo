4c9f9a3773287ccfc9704f9a9612a85db9e42114
mayan1303 <jindalmayan1303@gmail.com>
2026-03-04 23:27:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
-import threading
+import redis
 import time
 import os
+from redis_lock import RedisLock
 
 app = Flask(__name__)
 
-lock = threading.Lock()
+# Connect to Redis using Railway environment variable
+redis_client = redis.from_url(os.environ.get("REDIS_URL"))
+
+lock = RedisLock(redis_client)
 
 @app.route("/")
 def home():
@@ -14,18 +18,20 @@ def home():
 @app.route("/process")
 def process():
 
-    if not lock.acquire(blocking=False):
+    lock_id = lock.acquire_lock("my_resource")
+
+    if not lock_id:
         return "Resource is locked by another process"
 
     print("Lock acquired")
 
     time.sleep(5)
 
-    lock.release()
+    lock.release_lock("my_resource", lock_id)
 
     return "Process completed and lock released"
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
